77c7cd7ad2f351d148ccc793a0c34a18f6edc756
Patrick Luwum <luwyxx@gmail.com>
2017-08-30 02:22:33 +0300

    Add store method to store new task form input into database

--- app/Http/Controllers/MyApp/TaskController.php
+++ app/Http/Controllers/MyApp/TaskController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\MyApp;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\MyApp\Task;
+use Request;
 
 class TaskController extends Controller
 {
     public function create(){
         return view('MyApp.create');
+    }
+
+    public function store(){
+        $input = Request::all();
+        //TODO: Remove manual user_id when auth is implemented
+        $input['user_id'] = 1;
+        $task = Task::create($input);
+        return $task;
     }
 }
