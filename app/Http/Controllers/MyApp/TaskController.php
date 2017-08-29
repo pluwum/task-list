2e41148865adc06959f6701e76422c506350a757
Patrick Luwum <luwyxx@gmail.com>
2017-08-30 02:35:44 +0300

    Add edit method to show edit task page

--- app/Http/Controllers/MyApp/TaskController.php
+++ app/Http/Controllers/MyApp/TaskController.php
@@ -9,15 +9,27 @@ use Request;
 class TaskController extends Controller
 {
 
+    /**
+     * Display all tasks viewable by user
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
     public function index(){
         $tasks = Task::all();
         return $tasks;
     }
 
+    /**
+     * Display task create page
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create(){
         return view('MyApp.create');
     }
 
+    /**
+     * Persist a new task the database
+     * @return $this|\Illuminate\Database\Eloquent\Model
+     */
     public function store(){
         $input = Request::all();
         //TODO: Validate form input data before persist
@@ -27,7 +39,21 @@ class TaskController extends Controller
         return $task;
     }
 
+    /**
+     * Show details of a single task
+     * @param $task
+     * @return mixed
+     */
     public function show($task){
         return $task;
+    }
+
+    /**
+     * Show Task for editing
+     * @param $task
+     * @return $this
+     */
+    public function edit($task){
+        return view('MyApp.edit')->with(compact('task'));
     }
 }
