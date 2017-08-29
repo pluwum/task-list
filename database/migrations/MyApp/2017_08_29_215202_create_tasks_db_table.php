78d3f54ec6b2d512b50bf274e32fe6630489727f
Patrick Luwum <luwyxx@gmail.com>
2017-08-30 01:12:47 +0300

    Add user_id column to task table for mapping user to task

--- database/migrations/MyApp/2017_08_29_215202_create_tasks_db_table.php
+++ database/migrations/MyApp/2017_08_29_215202_create_tasks_db_table.php
@@ -18,6 +18,7 @@ class CreateTasksDbTable extends Migration
             $table->string('name');
             $table->string('description');
             $table->integer('state_id')->default(1);
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
