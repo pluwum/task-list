6ccadaabe5a9efdc933dd9dbea359987cfb49585
Patrick Luwum <luwyxx@gmail.com>
2017-08-30 02:13:06 +0300

    Add create task task function to display task creation form

--- app/Http/Controllers/MyApp/TaskController.php
+++ app/Http/Controllers/MyApp/TaskController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\MyApp;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\MyApp\Task;
 
 class TaskController extends Controller
 {
-    //
+    public function create(){
+        return view('MyApp.create');
+    }
 }
