77d7f3519d88d795d71e78c62bbbb04e188a8ac8
Patrick Luwum <luwyxx@gmail.com>
2017-08-30 01:10:09 +0300

    Add database table migrations for Task, User and password reset table

--- database/migrations/MyApp/2017_08_29_213000_create_users_db_table.php
+++ database/migrations/MyApp/2017_08_29_213000_create_users_db_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUsersDbTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('role')->default(2);
             $table->rememberToken();
             $table->timestamps();
         });
