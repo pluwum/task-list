6382525a737cd0f98e740c8eaa96d03435ad11b9
Patrick Luwum <luwyxx@gmail.com>
2017-08-31 01:19:25 +0300

    Add user table seeder to create default admin user

---
+++ database/seeds/userTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+class userTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Lets insert a default admin account for demo purpose
+        DB::table('users')->truncate();
+        DB::table('users')->insert([
+                [
+
+                    'first_name' => 'Super',
+                    'last_name' => 'User',
+                    'email' => 'admin@demo.com',
+                    'role' => '1',
+                    'password' => bcrypt('123456'),
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ]
+            ]
+        );
+    }
+}
