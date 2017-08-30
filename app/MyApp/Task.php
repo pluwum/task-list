818ad144f07effecda128def58cd87dc34c596a6
Patrick Luwum <luwyxx@gmail.com>
2017-08-30 03:41:13 +0300

    Add viewablebyMe query scope to filter tasks seen by user depending on the role

--- app/MyApp/Task.php
+++ app/MyApp/Task.php
@@ -3,7 +3,7 @@
 namespace App\MyApp;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Auth;
 class Task extends Model
 {
     //We define the mass assignable columns
@@ -17,5 +17,14 @@ class Task extends Model
     //Define user-task relationship
     public function user(){
         return $this->belongsTo('App\User');
+    }
+    // Filter the tasks user can access
+    public function scopeViewableByMe($query){
+        $user = Auth::user();
+        //Check if user is an admin
+        if($user->role == 1)
+            return $query->get();
+        // Return filtered if user is regular
+        return $query->where('user_id', $user->id)->get();
     }
 }
