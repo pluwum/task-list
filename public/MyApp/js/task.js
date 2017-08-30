99f5603d1e356903d2bae4319b43eb5678b2a32b
Patrick Luwum <luwyxx@gmail.com>
2017-08-30 21:21:30 +0300

    Add implementation to perform ajax calls to server for state change

--- public/MyApp/js/task.js
+++ public/MyApp/js/task.js
@@ -3,14 +3,26 @@ var Task = function() {
     return {
 
         initStateChange: function() {
+            // Set up the CSRF token for the ajax request to come
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
 
+            // This function changes the state of a task
             var changeState = function(task, new_state) {
-                //jQuery('.vmaps').hide();
-                alert('hey');
+                $.ajax({
+                    url: "/task/"+task+"/state",
+                    method: "POST",
+                    data: { state_id : new_state }
+                });
             }
 
-            jQuery('.task').change(function() {
-                changeState("world");
+            jQuery('.task-status').change(function() {
+                var task_id = $(this).attr('data-task');
+                var new_state = $(this).val();
+                changeState(task_id, new_state);
             });
         },
 
@@ -21,7 +33,6 @@ var Task = function() {
     };
 
 }();
-
 
 jQuery(document).ready(function() {
     // initialise our Task App
