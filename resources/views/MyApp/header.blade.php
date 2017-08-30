50d5d9fe25097b114206806ab43a793f5940914f
Patrick Luwum <luwyxx@gmail.com>
2017-08-30 21:24:28 +0300

    Add restriction on items displayed based on auth status

--- resources/views/MyApp/header.blade.php
+++ resources/views/MyApp/header.blade.php
@@ -3,6 +3,7 @@
 <head>
     <link rel="shortcut icon" href="{{ asset('assets/img/favicon.png') }}" type="image/png">
     <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Task List</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,15 +20,31 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/task">Task List App</a>
+            <a class="navbar-brand" href="#">Task List App</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
+            @if (!Auth::guest())
             <ul class="nav navbar-nav">
                 <li class="active"><a href="/task">View Lists</a></li>
                 <li><a href="/task/create">New List</a></li>
             </ul>
+            @endif
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="/logout">Logout</a></li>
+                @if (Auth::guest())
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                <li>
+                    <a href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profile<span class="caret"></span></a>
                     <ul class="dropdown-menu">
@@ -37,6 +54,7 @@
                         <li><a href="/logout">Logout</a></li>
                     </ul>
                 </li>
+                @endif
             </ul>
         </div><!--/.nav-collapse -->
     </div>
