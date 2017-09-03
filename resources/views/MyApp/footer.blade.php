5293aa5b9324a2220270ad14adfd52e3a59b02ee
Patrick Luwum <luwyxx@gmail.com>
2017-09-03 17:14:12 +0300

    Include External JS from CDN to enable datatables and buttons extension

--- resources/views/MyApp/footer.blade.php
+++ resources/views/MyApp/footer.blade.php
@@ -1,9 +1,34 @@
 
         </div>
 <!-- /container -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="{{asset('MyApp/js/task.js')}}"></script>
-</body>
+<!--data tables-->
+        <script type="text/javascript" language="javascript" src="//code.jquery.com/jquery-1.12.4.js">
+        </script>
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js">
+        </script>
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js">
+        </script>
+
+        <!-- datable-buttons extension-->
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.4.1/js/dataTables.buttons.min.js">
+        </script>
+        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.4.1/js/buttons.bootstrap.min.js">
+        </script>
+        <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.4.1/js/buttons.flash.min.js">
+        </script>
+        <script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js">
+        </script>
+        <script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js">
+        </script>
+        <script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js">
+        </script>
+        <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.4.1/js/buttons.html5.min.js">
+        </script>
+        <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.4.1/js/buttons.print.min.js">
+        </script>
+        <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.4.1/js/buttons.colVis.min.js">
+        </script>
+        <!--page JS-->
+        <script src="{{asset('MyApp/js/task.js')}}"></script>
+        </body>
 </html>
