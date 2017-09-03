a20477fc925b85d3772cadb7eee0e1b6124fc872
Patrick Luwum <luwyxx@gmail.com>
2017-09-03 19:05:21 +0300

    Remove datatables Button extension and Add table Export plugin

--- resources/views/MyApp/footer.blade.php
+++ resources/views/MyApp/footer.blade.php
@@ -2,32 +2,18 @@
         </div>
 <!-- /container -->
 <!--data tables-->
-        <script type="text/javascript" language="javascript" src="//code.jquery.com/jquery-1.12.4.js">
+        <script type="text/javascript" language="javascript" src="//code.jquery.com/jquery-2.1.1.js">
         </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
         <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js">
         </script>
         <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js">
         </script>
 
-        <!-- datable-buttons extension-->
-        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.4.1/js/dataTables.buttons.min.js">
-        </script>
-        <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.4.1/js/buttons.bootstrap.min.js">
-        </script>
-        <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.4.1/js/buttons.flash.min.js">
-        </script>
-        <script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js">
-        </script>
-        <script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js">
-        </script>
-        <script type="text/javascript" language="javascript" src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js">
-        </script>
-        <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.4.1/js/buttons.html5.min.js">
-        </script>
-        <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.4.1/js/buttons.print.min.js">
-        </script>
-        <script type="text/javascript" language="javascript" src="//cdn.datatables.net/buttons/1.4.1/js/buttons.colVis.min.js">
-        </script>
+        <!--table export plugin-->
+        <script type="text/javascript" src="{{asset('MyApp/js/plugins/tableexport/tableExport.js')}}"></script>
+        <script type="text/javascript" src="{{asset('MyApp/js/plugins/tableexport/jquery.base64.js')}}"></script>
+        <script type="text/javascript" src="{{asset('MyApp/js/plugins/tableexport/html2canvas.js')}}"></script>
         <!--page JS-->
         <script src="{{asset('MyApp/js/task.js')}}"></script>
         </body>
