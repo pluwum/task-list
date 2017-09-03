7fd39a57d5e0895ad3878ad974808c50b767f6ee
Patrick Luwum <luwyxx@gmail.com>
2017-09-03 17:15:16 +0300

    Change HTML table defination for datatables compatibility

--- resources/views/MyApp/index.blade.php
+++ resources/views/MyApp/index.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
     <div class="col-md-10 col-md-offset-1">
         <div class="panel panel-default">
-            <div class="panel-heading">All Tasks</div>
+            <div class="panel-heading" id="task_table_heading">All Tasks</div>
 
             <div class="panel-body">
                 @if (session('status'))
@@ -11,38 +11,42 @@
                         {{ session('status') }}
                     </div>
                 @endif
-                <table class="table">
-                    <tr>
-                        <th>#</th>
-                        <th>Task Name</th>
-                        <th>Description</th>
-                        <th>Status</th>
-                        <th>Actions</th>
-                    </tr>
-                    @foreach( $tasks as $task)
+                    <table id="task_table" class="table table-striped table-bordered">
+                        <thead>
                         <tr>
-                            <td>{{$loop->iteration}}</td>
-                            <td>{{$task->name}}</td>
-                            <td>{{$task->description}}</td>
-                            <td>
-                                {{Form::model($task)}}
-                                    {!! Form::select('state_id',[1=>'New',2=>'In Progress',3=>'Finished'],null,['class'=>'task-status form-control', 'data-task'=>$task->id])!!}
-                                {{Form::close()}}
-                            </td>
-                            <td>
-                                <div class="dropdown">
-                                    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Actions
-                                        <span class="caret"></span></button>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="" href="{{asset("task/$task->id/edit")}}">Edit</a></li>
-                                        <!-- TODO:Make Use DELETE instead of GET-->
-                                        <li><a class="" href="{{asset("task/$task->id/delete")}}">Delete</a></li>
-                                    </ul>
-                                </div>
-                            </td>
+                            <th>#</th>
+                            <th>Task Name</th>
+                            <th>Description</th>
+                            <th>Status</th>
+                            <th>Actions</th>
                         </tr>
-                    @endforeach
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach( $tasks as $task)
+                            <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$task->name}}</td>
+                                <td>{{$task->description}}</td>
+                                <td>
+                                    {{Form::model($task)}}
+                                    {!! Form::select('state_id',[1=>'New',2=>'In Progress',3=>'Finished'],null,['class'=>'task-status form-control', 'data-task'=>$task->id])!!}
+                                    {{Form::close()}}
+                                </td>
+                                <td>
+                                    <div class="dropdown">
+                                        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Actions
+                                            <span class="caret"></span></button>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="" href="{{asset("task/$task->id/edit")}}">Edit</a></li>
+                                            <!-- TODO:Make Use DELETE instead of GET-->
+                                            <li><a class="" href="{{asset("task/$task->id/delete")}}">Delete</a></li>
+                                        </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
             </div>
         </div>
     </div>
